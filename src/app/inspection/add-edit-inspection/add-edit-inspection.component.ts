cfbb21f8e35aa54ce9bff6f36d515063aaf3a677
Javier Paez <javierignacio.paez@gmail.com>
2022-07-07 12:19:23 -0300

    add new inspection button

--- src/app/inspection/add-edit-inspection/add-edit-inspection.component.ts
+++ src/app/inspection/add-edit-inspection/add-edit-inspection.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable, observable } from 'rxjs';
+import { InspectionApiService } from 'src/app/inspection-api.service';
 
 @Component({
   selector: 'app-add-edit-inspection',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditInspectionComponent implements OnInit {
 
-  constructor() { }
+  inspectionList$!: Observable<any[]>;
+  statusList$!: Observable<any[]>;
+  inspectionTypesList$!: Observable<any[]>;
+
+  constructor(private service: InspectionApiService) { }
+
+  @Input() inspection: any;
+  id: number = 0;
+  status: string = '';
+  comment: string = '';
+  inspectionTypeId!: number;
 
   ngOnInit(): void {
+    this.id = this.inspection.id;
+    this.status = this.inspection.status;
+    this.comment = this.inspection.comment;
+    this.inspectionTypeId = this.inspection.inspectionTypeId;
+    this.statusList$ = this.service.getStatusList();
+    this.inspectionList$ = this.service.getInspectionList();
+    this.inspectionTypesList$ = this.service.getInspectionTypesList();
   }
-
 }
